8c76766f06d0cb95cb785a926ce349c6bdd3d281
Efren Carbajal <efren.carbajal@rockstarcoders.com>
2018-08-22 21:31:58 -0500

    Add movie theather survey

---
+++ surveys/movie_theather.rb
@@ -0,0 +1,26 @@
+# encoding: UTF-8
+survey "How to get kicked out of a movie theater survey", :default_mandatory => true do
+
+  section "How to get kicked out of a movie theater survey" do
+
+    question "When watching a movie u feeling bored ?.", :pick => :one
+    answer "Gets up and start screaming"
+    answer "Disturb the stranger sitting beside you"
+    answer "Make loud comment in the hall"
+    answer "Quietly just sleep on the seat"
+
+    question "Watching a movie ,a very scary scene comes ?", :pick => :one
+    answer "Start Screaming loud and disturb others"
+    answer "Bite the sitting next by stranger"
+    answer "Just run away and move out of the movie"
+    answer "Just sit n watch the movie"
+
+    question "When sitting with a stranger annoying you?", :pick => :one
+    answer "Start fighting with him/her"
+    answer "Irritates him/her"
+    answer "Make loud stupid comments"
+    answer "Watch the movie and adjust"
+
+  end
+end
+
