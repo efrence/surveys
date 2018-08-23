75804708bab61a8e65d7af35dbdc4efc6f582c9d
Efren Carbajal <efren.carbajal@rockstarcoders.com>
2018-08-22 20:41:07 -0500

    Rails barebones project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SurveyorUcsf::Application.config.secret_key_base = '97745d34f53ef081bafabdd2a4abf08e9e6614720c63dff3a6ab4cc13bb01dfbeba557a4d62943418346ad003b61d2c88fc7323b5eb33f9c819c37ec2b54e488'
